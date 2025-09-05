043c2d340b1eb7282f4790a3f52aa6acd3c7f63d
Gabriel <pro.gabrielteixeira@gmail.com>
2025-09-05 11:03:54 +0100

    remove console interaction to DOM interaction

--- script.js
+++ script.js
@@ -1,57 +1,56 @@
+const userChoiseRock = document.querySelector('#rock');
+const userChoisePaper = document.querySelector('#paper');
+const userChoiseScissors = document.querySelector('#scissors');
+
+const displayMatchResult = document.querySelector('#display-match-result')
+const UIComputerScore = document.querySelector('#computer-score');
+const UIHumanScore = document.querySelector('#human-score');
 
 const ROCK = 0;
 const PAPER = 1;
 const SCISSORS = 2;
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-	return parseInt(Math.random() * 10 / 3);
+	return Math.floor(Math.random() * 3);
 }
 
-function getHumanChoice() {
-	let choice = prompt("Rock, paper or scissors?").toUpperCase();
-	return choice == "ROCK" ? 0 : choice == "PAPER" ? 1 : 2; 
+function getChoiceName(choice) {
+	return (choice === ROCK) ? "rock" : (choice === PAPER) ? "paper" : "scissors";
 }
 
 function playRound(humanChoice, computerChoice) {
-	if (humanChoice > computerChoice) {
-		if (computerChoice == 1) {
-			console.log("Human won! Scissors beats paper");
+	if ((humanChoice > computerChoice && (computerChoice == 1 || humanChoice != 2)) || computerChoice == 2 && humanChoice == 0) {
+			displayMatchResult.textContent = `Human won - ${getChoiceName(humanChoice)} beats ${getChoiceName(computerChoice)}`;
 			return 1;
-		} 
-		else if (humanChoice == 2) {
-			console.log("Computer won! Rock beats scissors!");
-			return 0
 		}
-		console.log("Human won! Paper beats rock");
-		return 1;
-	}
-	if (humanChoice == 1) {
-		console.log("Computer won! Scissors beats paper");
-		return 0;
-	}
-	else if (computerChoice == 2) {
-		console.log("Human won! Rock beats scissors!");
-		return 1
-	}
-	console.log("Computer won! Paper beats rock");
+	displayMatchResult.textContent = `Computer won - ${getChoiceName(computerChoice)} beats ${getChoiceName(humanChoice)}`;
 	return 0;
 }
 
-
-function playGame() {
-
-	let humanScore = 0;
-	let computerScore = 0;
-	
-	while (humanScore == 5 || computerScore == 5) {
-		const humanChoice = getHumanChoice();
+function playGame(humanChoice) {
 		const computerChoice = getComputerChoice();
 		(humanChoice == computerChoice) ? 
-			console.log("draw") : (playRound(humanChoice, computerChoice)) ? 
-			humanScore++ : computerScore++;
-		console.log(`Computer score: ${computerScore} VS. Human score: ${humanScore}`)
-	}
-	(humanScore > computerScore) ? console.log("You won the Game!! Congrats!") : (humanScore < computerScore) ? console.log("The computer won the game! Sorry. Don't cry.") : console.log("It's a draw!!")
+			displayMatchResult.textContent = "Draw" : (playRound(humanChoice, computerChoice)) ? 
+			UIHumanScore.textContent = ++humanScore : UIComputerScore.textContent = ++computerScore;
+		if (humanScore == 5 || computerScore == 5) {
+			(humanScore == 5) ? displayMatchResult.textContent = 'YOU WON the game!! Congratulations!' :
+				displayMatchResult.textContent = 'You lose the game!! Sorry!'
+			humanScore = 0;
+			UIHumanScore.textContent = 0;
+			computerScore = 0;
+			UIComputerScore.textContent = 0;
+		}
 }
 
-playGame();
+document.addEventListener('click', (e) => {
+	const target = e.target;
+	if (target === userChoiseRock)
+		playGame(0);
+	else if (target === userChoisePaper)
+		playGame(1);
+	else if (target === userChoiseScissors)
+		playGame(2);
+})
